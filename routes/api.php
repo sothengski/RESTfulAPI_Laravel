098be68fdc1f2c65f16945639775d75dcb55f97e
SothengSki <chheangsotheng@gmail.com>
2020-04-19 21:41:28 +0700

    implement product-Buyer,BuyerTransaction,Category,transaction method

--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,15 @@ Route::resource('categories.transactions', 'Category\CategoryTransactionControll
  * Products
  */
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
+Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
+Route::resource('products.buyers.transactions', 'Product\ProductBuyerTransactionController', ['only' => ['store']]);
+
+
+
+
+
 /**
  * Sellers
  */
